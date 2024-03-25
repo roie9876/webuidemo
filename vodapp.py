dbc78b883adba8d85c0247cf502b619fec33f63b
John Doe <johndoe@email.com>
2024-03-25 20:49:24 +0200

    remove openai

--- vodapp.py
+++ vodapp.py
@@ -8,8 +8,8 @@ import azure.cosmos.exceptions as exceptions
 from azure.cosmos.http_constants import StatusCodes
 from azure.cosmos.partition_key import PartitionKey
 from azure.cosmos import CosmosClient, PartitionKey
-import openai
-from openai import AzureOpenAI
+#import openai
+#from openai import AzureOpenAI
 import streamlit as st
 
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
@@ -94,13 +94,13 @@ for item in items:
 
 
 
-client = AzureOpenAI(
-  api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
-  api_version = "2023-05-15",
-  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")  # Your Azure OpenAI resource's endpoint value.
-)
+# client = AzureOpenAI(
+#   api_key = os.getenv("AZURE_OPENAI_API_KEY"),  
+#   api_version = "2023-05-15",
+#   azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")  # Your Azure OpenAI resource's endpoint value.
+# )
 
-conversation=[{"role": "system", "content": "You are a helpful assistant."}]
+# conversation=[{"role": "system", "content": "You are a helpful assistant."}]
 
 # Assume `videos` is a list of dictionaries, each representing a video
 videos = container.query_items(
